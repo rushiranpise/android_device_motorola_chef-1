f9531a4d61a2a633609fca7a65604eea9b4b87a9
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-05-25 21:32:56 +0530

    Update lineage_chef.mk

--- lineage_chef.mk
+++ lineage_chef.mk
@@ -15,6 +15,15 @@
 #
 
 # Inherit some common Lineage stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GMS := true
+TARGET_ENABLE_BLUR := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
