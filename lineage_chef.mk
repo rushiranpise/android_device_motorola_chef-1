5bbe7488de120388f713de2220193340b574960a
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-05-22 17:44:31 +0000

    chef: Adapt to CrDroid

--- lineage_chef.mk
+++ lineage_chef.mk
@@ -21,6 +21,16 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+# Crdroid
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+
 # Device
 $(call inherit-product, device/motorola/chef/device.mk)
 
